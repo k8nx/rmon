bf0ab0f7cc811999dfd8ecebe8a6afd24426418b
Daegeun Kim <dgkim84@gmail.com>
2013-06-24 00:22:14 +0900

    flask static folder 변경 - static => assets

--- rmon/__init__.py
+++ rmon/__init__.py
@@ -4,7 +4,7 @@
 """
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='assets', static_url_path='/assets')
 app.config.from_envvar('RMON_SETTINGS')
 
 from rmon.commons.views import mod as commonsModule
