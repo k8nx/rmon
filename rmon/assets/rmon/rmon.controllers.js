f49dbf5796b6a4c54f1391bdf7d4777b1647642c
Daegeun Kim <dgkim84@gmail.com>
2013-06-28 23:51:17 +0900

    clusters 목록만 볼 수 있도록 기능 구현

--- rmon/assets/rmon/rmon.controllers.js
+++ rmon/assets/rmon/rmon.controllers.js
@@ -1,6 +1,9 @@
 define([], function() {
   function DashboardCtrl($scope, $http) {
+    $scope.clusters = [];
+
     $http.get('/v1/clusters/').success(function(json) {
+      $scope.clusters = json;
     });
   };
   DashboardCtrl.$inject = ['$scope', '$http'];
