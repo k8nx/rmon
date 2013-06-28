6d0b8ddb1e7d48ac14f6e196889ebd1c7221bb29
Daegeun Kim <dgkim84@gmail.com>
2013-06-29 02:49:54 +0900

    cluster 모델 필드추가 (node.info, cluster.type)

--- rmon/commons/models.py
+++ rmon/commons/models.py
@@ -1,8 +1,9 @@
 import json
 
 class Cluster:
-    def __init__(self, id, nodes):
+    def __init__(self, id, type, nodes):
         self.id = id
+        self.type = type
         self.nodes = nodes
 
     def __repr__(self):
@@ -13,6 +14,7 @@ class Node:
         self.name = name
         self.hostname = hostname
         self.port = port
+        self.info = None
 
     def __repr__(self):
         return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
--- rmon/loaders/dummy.py
+++ rmon/loaders/dummy.py
@@ -3,12 +3,12 @@ from rmon.commons.models import Cluster, Node
 
 class DummyLoader(Loader):
     def load(self):
-        cluster1 = Cluster('redis-cluster-1', [
+        cluster1 = Cluster('redis-cluster-1', 'redis', [
             Node('redis-1:1', 'localhost', 6379),
             Node('redis-1:2', 'localhost', 6380),
             Node('redis-1:3', 'localhost', 6381)
         ])
-        cluster2 = Cluster('redis-cluster-2', [
+        cluster2 = Cluster('redis-cluster-2', 'redis', [
             Node('redis-2:1', 'localhost', 7379),
             Node('redis-2:2', 'localhost', 7380),
             Node('redis-2:3', 'localhost', 7381)
