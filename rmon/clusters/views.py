23e11f23c012845866a7bb7da37e0100dd2352ab
Daegeun Kim <dgkim84@gmail.com>
2013-06-29 02:54:12 +0900

    1차 프로토타이핑

--- rmon/clusters/views.py
+++ rmon/clusters/views.py
@@ -11,6 +11,11 @@ mod = Blueprint('cluster', __name__, url_prefix='/v1/clusters')
 
 @mod.route('/')
 def index():
+    for cluster in clusters.clusters:
+        from rmon.processes.redis_process import Redis
+        for node in cluster.nodes:
+            process = Redis()
+            node.info = process.info(node)
     return repr(clusters.clusters)
 
 @mod.route('/<string:id>/nodes', methods=('GET','POST'))
