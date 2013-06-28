e163ed4290c4debd68f240f38da3a401710e2981
Daegeun Kim <dgkim84@gmail.com>
2013-06-28 23:27:23 +0900

    json serialize

--- rmon/commons/models.py
+++ rmon/commons/models.py
@@ -1,11 +1,18 @@
+import json
 
 class Cluster:
     def __init__(self, id, nodes):
         self.id = id
         self.nodes = nodes
 
+    def __repr__(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
 class Node:
     def __init__(self, name, hostname, port):
         self.name = name
         self.hostname = hostname
         self.port = port
+
+    def __repr__(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
